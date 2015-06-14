113a00a177bad6dd6352aaa8035adb31b47dfa54
enrique7mc <enrique7mc@gmail.com>
2015-06-13 20:13:32 -0500

    Tests the content provider
    Loads the notes to the ListView

--- SimpleNotes/app/src/main/java/com/enrique7mc/simplenotes/MainActivity.java
+++ SimpleNotes/app/src/main/java/com/enrique7mc/simplenotes/MainActivity.java
@@ -1,19 +1,45 @@
 package com.enrique7mc.simplenotes;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 
 public class MainActivity extends ActionBarActivity {
+	private static final String TAG = MainActivity.class.getSimpleName();
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		insertNote("New note");
+
+		Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI,
+				DBOpenHelper.ALL_COLUMNS, null, null, null, null);
+
+		String[] from = {DBOpenHelper.NOTE_TEXT};
+		int[] to = {android.R.id.text1};
+		CursorAdapter cursorAdapter = new SimpleCursorAdapter(this,
+				android.R.layout.simple_list_item_1, cursor, from, to, 0);
+
+		ListView list = (ListView) findViewById(android.R.id.list);
+		list.setAdapter(cursorAdapter);
+	}
+
+	private void insertNote(String noteText) {
+		ContentValues values = new ContentValues();
+		values.put(DBOpenHelper.NOTE_TEXT, noteText);
+		Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+		Log.d(TAG, "Inserted note " + noteUri.getLastPathSegment());
 	}
 
 	@Override
@@ -25,9 +51,6 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
