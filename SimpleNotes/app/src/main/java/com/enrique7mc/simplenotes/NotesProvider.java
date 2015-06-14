bd9893df6dcab6a4f0f70e4955afb95ebc9b5ccf
enrique7mc <enrique7mc@gmail.com>
2015-06-13 20:12:23 -0500

    Adds the implementation for the content provider CRUD operations

--- SimpleNotes/app/src/main/java/com/enrique7mc/simplenotes/NotesProvider.java
+++ SimpleNotes/app/src/main/java/com/enrique7mc/simplenotes/NotesProvider.java
@@ -37,8 +37,10 @@ public class NotesProvider extends ContentProvider {
 	}
 
 	@Override
-	public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
-		return null;
+	public Cursor query(Uri uri, String[] projection, String selection,
+						String[] selectionArgs, String sortOrder) {
+		return database.query(DBOpenHelper.TABLE_NOTES, DBOpenHelper.ALL_COLUMNS, selection,
+				null, null, null, DBOpenHelper.NOTE_CREATED + " DESC");
 	}
 
 	@Override
@@ -47,17 +49,19 @@ public class NotesProvider extends ContentProvider {
 	}
 
 	@Override
-	public Uri insert(Uri uri, ContentValues contentValues) {
-		return null;
+	public Uri insert(Uri uri, ContentValues values) {
+		long id = database.insert(DBOpenHelper.TABLE_NOTES, null, values);
+		return Uri.parse(BASE_PATH + "/" + id);
 	}
 
 	@Override
-	public int delete(Uri uri, String s, String[] strings) {
-		return 0;
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
+		return database.delete(DBOpenHelper.TABLE_NOTES, selection, selectionArgs);
 	}
 
 	@Override
-	public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
-		return 0;
+	public int update(Uri uri, ContentValues values, String selection,
+					  String[] selectionArgs) {
+		return database.update(DBOpenHelper.TABLE_NOTES, values, selection, selectionArgs);
 	}
 }
