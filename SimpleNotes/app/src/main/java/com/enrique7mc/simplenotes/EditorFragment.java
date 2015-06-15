138b4dae66b1b8bae560f962929491b1c12ee187
enrique7mc <enrique7mc@gmail.com>
2015-06-15 17:26:15 -0500

    Change project structure to use fragments

---
+++ SimpleNotes/app/src/main/java/com/enrique7mc/simplenotes/EditorFragment.java
@@ -0,0 +1,158 @@
+package com.enrique7mc.simplenotes;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Enrique on 14/06/2015.
+ */
+public class EditorFragment extends Fragment {
+	private String action;
+	private EditText editor;
+	private String noteFilter;
+	private String oldText;
+	private Uri uri;
+
+	public static EditorFragment newInstance(Parcelable uri) {
+		Bundle args = new Bundle();
+		args.putParcelable(NotesProvider.CONTENT_ITEM_TYPE, uri);
+
+		EditorFragment fragment = new EditorFragment();
+		fragment.setArguments(args);
+
+		return fragment;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_editor, container, false);
+		editor = (EditText) v.findViewById(R.id.editText);
+
+		uri = getArguments().getParcelable(NotesProvider.CONTENT_ITEM_TYPE);
+		if (uri == null) {
+			action = Intent.ACTION_INSERT;
+			getActivity().setTitle(getString(R.string.new_note));
+		} else {
+			action = Intent.ACTION_EDIT;
+			noteFilter = DBOpenHelper.NOTE_ID + "=" + uri.getLastPathSegment();
+
+			Cursor cursor = getActivity().getContentResolver().query(uri, DBOpenHelper.ALL_COLUMNS,
+					noteFilter, null, null);
+			cursor.moveToFirst();
+			oldText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+			editor.setText(oldText);
+			editor.requestFocus();
+		}
+
+		return v;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		if (action.equals(Intent.ACTION_EDIT)) {
+			inflater.inflate(R.menu.menu_editor, menu);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		switch (id){
+			case android.R.id.home:
+				finishEditing();
+				break;
+			case R.id.action_delete:
+				deleteNote();
+				getActivity().finish();
+				break;
+		}
+
+		return true;
+	}
+
+	private void deleteNote() {
+		getActivity().getContentResolver().delete(NotesProvider.CONTENT_URI, noteFilter, null);
+		Toast.makeText(getActivity(), getString(R.string.note_deleted), Toast.LENGTH_SHORT).show();
+		getActivity().setResult(Activity.RESULT_OK);
+	}
+
+	private void finishEditing() {
+		String newText = editor.getText().toString().trim();
+		switch (action){
+			case Intent.ACTION_INSERT:
+				if (newText.length() == 0) {
+					getActivity().setResult(Activity.RESULT_CANCELED);
+				} else{
+					insertNote(newText);
+				}
+				break;
+			case Intent.ACTION_EDIT:
+				if (newText.length() == 0){
+					deleteNote();
+				} else if(oldText.equals(newText)) {
+					getActivity().setResult(Activity.RESULT_CANCELED);
+				} else {
+					updateNote(newText);
+				}
+		}
+
+		getActivity().finish();
+	}
+
+	private void updateNote(String noteText) {
+		ContentValues values = new ContentValues();
+		values.put(DBOpenHelper.NOTE_TEXT, noteText);
+		getActivity().getContentResolver().update(NotesProvider.CONTENT_URI, values, noteFilter, null);
+		Toast.makeText(getActivity(), getString(R.string.note_updated), Toast.LENGTH_SHORT).show();
+		getActivity().setResult(Activity.RESULT_OK);
+	}
+
+	private void insertNote(String noteText) {
+		ContentValues values = new ContentValues();
+		values.put(DBOpenHelper.NOTE_TEXT, noteText);
+		getActivity().getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+		getActivity().setResult(Activity.RESULT_OK);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		getView().setFocusableInTouchMode(true);
+		getView().requestFocus();
+		getView().setOnKeyListener(new View.OnKeyListener() {
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK) {
+					finishEditing();
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+}
---
+++ SimpleNotes/app/src/main/java/com/enrique7mc/simplenotes/MainFragment.java
@@ -0,0 +1,161 @@
+package com.enrique7mc.simplenotes;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import at.markushi.ui.CircleButton;
+
+/**
+ * Created by Enrique on 14/06/2015.
+ */
+public class MainFragment extends Fragment
+		implements LoaderManager.LoaderCallbacks<Cursor>{
+	private CursorAdapter cursorAdapter;
+	private at.markushi.ui.CircleButton newButton;
+	private static final String TAG = MainActivity.class.getSimpleName();
+	public static final int EDITOR_REQUEST_CODE = 1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+		getLoaderManager().initLoader(0, null, this);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+		cursorAdapter = new NotesCursorAdapter(getActivity(), null, 0);
+		ListView list = (ListView) v.findViewById(android.R.id.list);
+		list.setAdapter(cursorAdapter);
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+				Intent intent = new Intent(getActivity(), EditorActivity.class);
+				Uri uri = Uri.parse(NotesProvider.CONTENT_URI + "/" + id);
+				intent.putExtra(NotesProvider.CONTENT_ITEM_TYPE, uri);
+				startActivityForResult(intent, EDITOR_REQUEST_CODE);
+			}
+		});
+		newButton = (CircleButton) v.findViewById(R.id.newButton);
+		newButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(getActivity(), EditorActivity.class);
+				startActivityForResult(intent, EDITOR_REQUEST_CODE);
+			}
+		});
+
+		return v;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.menu_main, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		switch (id){
+			case R.id.action_create_sample:
+				insertSampleData();
+				return true;
+			case R.id.action_delete_all:
+				deleteAllNotes();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void deleteAllNotes() {
+		DialogInterface.OnClickListener dialogClickListener =
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int button) {
+						if (button == DialogInterface.BUTTON_POSITIVE) {
+							getActivity().getContentResolver()
+									.delete(NotesProvider.CONTENT_URI, null, null);
+							restartLoader();
+
+							Toast.makeText(getActivity(),
+									getString(R.string.all_deleted),
+									Toast.LENGTH_SHORT).show();
+						}
+					}
+				};
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setMessage(getString(R.string.are_you_sure))
+				.setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+				.setNegativeButton(getString(android.R.string.no), null)
+				.show();
+	}
+
+	private void insertSampleData() {
+		insertNote("Simple note");
+		insertNote("Multiline\nnote");
+		insertNote("Very long note, this note has a lot of text that exceeds the" +
+				"width of the screen");
+		restartLoader();
+	}
+
+	private void restartLoader() {
+		getLoaderManager().restartLoader(0, null, this);
+	}
+
+	private void insertNote(String noteText) {
+		ContentValues values = new ContentValues();
+		values.put(DBOpenHelper.NOTE_TEXT, noteText);
+		Uri noteUri = getActivity().getContentResolver().insert(NotesProvider.CONTENT_URI, values);
+		Log.d(TAG, "Inserted note " + noteUri.getLastPathSegment());
+	}
+
+	@Override
+	public android.support.v4.content.Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(getActivity(), NotesProvider.CONTENT_URI, null, null, null, null);
+	}
+
+	@Override
+	public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor data) {
+		cursorAdapter.swapCursor(data);
+	}
+
+	@Override
+	public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
+		cursorAdapter.swapCursor(null);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == EDITOR_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+			restartLoader();
+		}
+	}
+}
