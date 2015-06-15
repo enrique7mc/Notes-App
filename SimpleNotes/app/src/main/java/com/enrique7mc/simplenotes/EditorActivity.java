27130c2c32fb6a3f485b26d22f5fd5a9bb142da7
enrique7mc <enrique7mc@gmail.com>
2015-06-15 17:49:28 -0500

    Adds a field for OnBackPressedListener

--- SimpleNotes/app/src/main/java/com/enrique7mc/simplenotes/EditorActivity.java
+++ SimpleNotes/app/src/main/java/com/enrique7mc/simplenotes/EditorActivity.java
@@ -5,10 +5,25 @@ import android.support.v4.app.Fragment;
 
 
 public class EditorActivity extends SingleFragmentActivity {
+	protected OnBackPressedListener onBackPressedListener;
 
 	@Override
 	protected Fragment createFragment() {
 		Uri uri = getIntent().getParcelableExtra(NotesProvider.CONTENT_ITEM_TYPE);
 		return EditorFragment.newInstance(uri);
+	}
+
+	public void setOnBackPressedListener(OnBackPressedListener onBackPressedListener) {
+		this.onBackPressedListener = onBackPressedListener;
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (onBackPressedListener != null) {
+			onBackPressedListener.doBack();
+		}
+		else {
+			super.onBackPressed();
+		}
 	}
 }
